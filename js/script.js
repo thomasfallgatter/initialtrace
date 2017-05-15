1bbbd21ba89e34bbe1632dddd05d2c5b71ba185b
Estefanio NS <estefanions@gmail.com>
2017-05-15 17:15:39 -0300

    update

--- js/script.js
+++ js/script.js
@@ -83,7 +83,20 @@ $('.menu_sidebar li.has_dropdown>a').on('click', function(){
 	}
 
 
+var headertext = [],
+headers = document.querySelectorAll(".tb_scale th"),
+tablerows = document.querySelectorAll(".tb_scale th"),
+tablebody = document.querySelector(".tb_scale tbody");
 
+for(var i = 0; i < headers.length; i++) {
+  var current = headers[i];
+  headertext.push(current.textContent.replace(/\r?\n|\r/,""));
+} 
+for (var i = 0, row; row = tablebody.rows[i]; i++) {
+  for (var j = 0, col; col = row.cells[j]; j++) {
+    col.setAttribute("data-th", headertext[j]);
+  } 
+}
 
 
 
